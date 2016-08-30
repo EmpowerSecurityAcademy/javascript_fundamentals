2e833f469020c974ae9b75c6592d972c7b66449a
sheltowt <sheltowt@gmail.com>
2016-08-30 12:33:32 -0400

    adding updates

--- koans/AboutStrings.js
+++ koans/AboutStrings.js
@@ -1,4 +1,4 @@
-describe("About Objects", function () {
+describe("About Strings", function () {
 
   describe("Properties", function () {
     var megalomaniac;
@@ -30,80 +30,5 @@ describe("About Objects", function () {
 
     var battleCry = megalomaniac.battleCry(4);
     expect("They are Pinky and the Brain Brain Brain Brain").toMatch(battleCry);
-  });
-
-  it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
-    var currentDate = new Date();
-    var currentYear = (currentDate.getFullYear());
-    var megalomaniac = {
-      mastermind: "James Wood",
-      henchman: "Adam West",
-      birthYear: 1970,
-      calculateAge: function () {
-        return currentYear - this.birthYear;
-      }
-    };
-
-    expect(currentYear).toBe(2016);
-    expect(megalomaniac.calculateAge()).toBe(46);
-  });
-
-  describe("'in' keyword", function () {
-    var megalomaniac;
-    beforeEach(function () {
-      megalomaniac = {
-        mastermind: "The Monarch",
-        henchwoman: "Dr Girlfriend",
-        theBomb: true
-      };
-    });
-
-    it("should have the bomb", function () {
-
-      var hasBomb = "theBomb" in megalomaniac;
-
-      expect(hasBomb).toBe(true);
-    });
-
-    it("should not have the detonator however", function () {
-
-      var hasDetonator = "theDetonator" in megalomaniac;
-
-      expect(hasDetonator).toBe(false);
-    });
-  });
-
-  it("should know that properties can be added and deleted", function () {
-    var megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
-
-    expect("secretary" in megalomaniac).toBe(false);
-
-    megalomaniac.secretary = "Agent Smith";
-    expect("secretary" in megalomaniac).toBe(true);
-
-    delete megalomaniac.henchman;
-    expect("henchman" in megalomaniac).toBe(false);
-  });
-
-
-  it("should use prototype to add to all objects", function () {
-      function Circle(radius)
-      {
-        this.radius = radius;
-      }
-
-      var simpleCircle = new Circle(10);
-      var colouredCircle = new Circle(5);
-      colouredCircle.colour = "red";
-
-      expect(simpleCircle.colour).toBe(undefined);
-      expect(colouredCircle.colour).toBe("red");
-
-      Circle.prototype.describe = function () {
-        return "This circle has a radius of: " + this.radius;
-      };
-
-      expect(simpleCircle.describe()).toBe("This circle has a radius of: 10");
-      expect(colouredCircle.describe()).toBe("This circle has a radius of: 5");
   });
 });
